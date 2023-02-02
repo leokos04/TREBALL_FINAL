addb17fcafdcd3516bfc79944b5ad94a92bcc98c
leokos04 <leonardo.d.kostadinov@gmail.com>
2023-02-02 10:38:58 +0100

    reservas listo

--- server/crud/deleteReservasUser.php
+++ server/crud/deleteReservasUser.php
@@ -1,2 +1,16 @@
 <?php
+include_once __DIR__ . "/../constant.php";
+require_once ROOT_PATH . "connection/conexion.php";
+require_once ROOT_PATH . "crud/crud.php";
 
+$idMusica = $_POST["id_musica"];
+$idReserva = $_POST["id_reserva"];
+
+if(!empty($idMusica) && !empty($idReserva)){
+  $bbdd = new Crud();
+  $bbdd->delReservaUserMusic($idReserva,$idMusica);
+  echo true;
+  exit();
+}
+echo false;
+exit();
